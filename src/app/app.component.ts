15ca6c760ed46918b503fc627ab85ae1922e2112
Gabriel <gabriel@hipcomp.com.br>
2020-12-17 22:30:03 -0300

    posicionamento dos itens no mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isCollapsed = false;
+  openMap: { [name: string]: boolean } = {
+    sub1: true,
+    sub2: false,
+    sub3: false
+  };
+
+  openHandler(value: string): void {
+    for (const key in this.openMap) {
+      if (key !== value) {
+        this.openMap[key] = false;
+      }
+    }
+  }
 }
